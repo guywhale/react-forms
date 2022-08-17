63d65b2983410509381c86721e1f0466bdb5e8b7
Guy Whale <guywhale@gmail.com>
2022-08-17 15:01:12 +0100

    Refactor code so that entered name validity state is normal variable that simply evaluates the entered name state when it changes on component re-render. This makes the code leaner and easier to read.

--- src/components/SimpleInput.js
+++ src/components/SimpleInput.js
@@ -1,4 +1,4 @@
-import { useState, useRef } from 'react';
+import { useState } from 'react';
 
 /**
  * Two approaches to validating input:
@@ -8,66 +8,51 @@ import { useState, useRef } from 'react';
  */
 
 const SimpleInput = (props) => {
-	// Ref approach
-	const nameInputRef = useRef();
 	//State approach
 	const [enteredName, setEnteredName] = useState('');
-	const [enteredNameIsValid, setEnteredNameIsValid] = useState(false);
 	const [enteredNameTouched, setEnteredNameTouched] = useState(false);
+
+	/**
+	 * To make code leaner, set enteredNameIsValid by evaluating enteredName.
+	 *
+	 * As input events change enteredName and enteredNameTouched states, they
+	 * trigger a component re-render meaning the enteredNameIsValid will be
+	 * adjusted accordingly.
+	 */
+	const enteredNameIsValid = enteredName.trim() !== '';
+	const nameInputIsInvalid = !enteredNameIsValid && enteredNameTouched;
 
 	const nameInputChangeHander = (event) => {
 		setEnteredName(event.target.value);
-
-		/**
-		 * You can't check enteredName to check validity here because
-		 * setEnteredName() has been run in the line above, meaning
-		 * React has only scheduled this state change to happen and not
-		 * actually made it.
-		 *
-		 * Instead, use the event value to check validity.
-		 */
-		if (event.target.value.trim() !== '') {
-			setEnteredNameIsValid(true);
-		}
 	};
 
 	const nameInputBlurHandler = (event) => {
 		setEnteredNameTouched(true);
-
-		if (enteredName.trim() === '') {
-			setEnteredNameIsValid(false);
-		}
 	};
 
 	const formSubmissionHandler = (event) => {
 		event.preventDefault();
 		setEnteredNameTouched(true);
 
-		// Value taken from input change handler
-		console.log(enteredName);
-
-		if (enteredName.trim() === '') {
-			setEnteredNameIsValid(false);
+		if (!enteredNameIsValid) {
 			return;
 		}
 
-		setEnteredNameIsValid(true);
-
-		// Value taken from input ref on submission
-		const enteredValue = nameInputRef.current.value;
-		console.log(enteredValue);
+		// Value taken from input change handler
+		console.log(enteredName);
 
 		/**
 		 * How to reset input after submission...
+		 * Set to blank and put value={enteredName} as in put attribute
 		 */
-		// NOT a good idea to set ref value yourself as only React should change
-		// the DOM
-		nameInputRef.current.value = '';
-		// Set to blank and put value={enteredName} as in put attribute
 		setEnteredName('');
-	};
 
-	const nameInputIsInvalid = !enteredNameIsValid && enteredNameTouched;
+		/**
+		 * After form submit touched state reset to false, as the data is
+		 * cleared and a fresh submission process should begin.
+		 */
+		setEnteredNameTouched(false);
+	};
 
 	const nameInputClasses = nameInputIsInvalid
 		? 'form-control invalid'
@@ -82,7 +67,6 @@ const SimpleInput = (props) => {
 					id="name"
 					onChange={nameInputChangeHander}
 					onBlur={nameInputBlurHandler}
-					ref={nameInputRef}
 					value={enteredName}
 				/>
 				{nameInputIsInvalid && (
